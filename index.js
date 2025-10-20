bb027fcb70778b3a7413cb2fa95a525ca699a8fa
Liliia bc2025-4 <liliyaustrytska@gmail.com>
2025-10-21 00:02:27 +0300

    Part 2 done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Command } = require('commander');
 const fs = require ('fs');
+const fsp = require('fs').promises;
 const http = require ('http');
+const { XMLBuilder } = require('fast-xml-parser');
 
 const program = new Command();
 
@@ -23,9 +25,39 @@ if (!options.input || !options.host || !options.port){
   process.exit(1);
 }
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Сервер працює успішно!');
+const parser = new XMLBuilder({ ignoreAttributes: false, format: true });
+
+const server = http.createServer(async(req, res) => {
+  try{
+   const rawData = await fsp.readFile(options.input, 'utf8');
+    const lines = rawData.split('\n').filter(line => line.trim() !== '');
+    const cars = lines.map(line => JSON.parse(line));
+
+    const url = new URL(req.url, `http://${options.host}:${options.port}`);
+    const showCylinders = url.searchParams.get('cylinders') === 'true';
+    const maxMpg = parseFloat(url.searchParams.get('max_mpg'));
+
+    let filteredCars = cars;
+        if (!isNaN(maxMpg)) {
+      filteredCars = filteredCars.filter(car => car.mpg < maxMpg);
+    }
+
+    const resultCars = filteredCars.map(car => {
+      const obj = { model: car.model, mpg: car.mpg };
+      if (showCylinders) obj.cyl = car.cyl;
+      return obj;
+    });
+
+    const xmlData = parser.build({ cars: { car: resultCars } }); 
+    
+    res.writeHead(200, { 'Content-Type': 'application/xml; charset=utf-8' });
+    res.end(xmlData);
+  } catch (err){
+     res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('Помилка при читанні або обробці файлу');
+    console.error(err);
+  }
+ 
 });
 
 server.listen(options.port, options.host, () => {
