ee351f83e47700a6e1e1e593d3942b50c7a941e6
Liliia bc2025-4 <liliyaustrytska@gmail.com>
2025-10-20 15:53:58 +0300

    Implement part 1: common CLI parameters and HTTP server

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const { program } = require('commander');
+const { Command } = require('commander');
+const fs = require ('fs');
+const http = require ('http');
+
+const program = new Command();
 
 program
-  .option('-n, --name <string>', 'User name')
-  .option('-a, --age <number>', 'User age');
-
+  .option('-i, --input <path>', '(обовʼязковий параметр) шлях до файлу, який даємо для читання')
+  .option('-h, --host <host>', '(обовʼязковий параметр) адреса сервера')
+  .option('-p, --port <port>', '(обовʼязковий параметр) порт сервера');
+  
 program.parse(process.argv);
 
 const options = program.opts();
-console.log(`Name: ${options.name}, Age: ${options.age}`);
+
+if(!fs.existsSync(options.input)){
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+
+if (!options.input || !options.host || !options.port){
+  console.error('Error: Missing required parameter');
+  process.exit(1);
+}
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Сервер працює успішно!');
+});
+
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}/`)
+})
